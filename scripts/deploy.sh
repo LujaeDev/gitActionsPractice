3003584ac2d8be9b16b50b7763a185c5f310669c
youjae <youjae0803@gmail.com>
2023-10-27 22:30:28 +0900

    update APP_NAME (gitactions -> gitActionsPractice)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 REPOSITORY=/home/ubuntu/gitActionsPractice
 cd $REPOSITORY
 
-APP_NAME=gitactions
+APP_NAME=gitActionsPractice
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
